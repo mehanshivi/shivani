624695137696733daf3f21fc37e6f77fc6c37d95
shivani <shivanimehan2@gmail.com>
2015-10-21 16:38:20 +0530

    intializing inputdata function

---
+++ PersonalDetails.c
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void inputdata()
+{
+	string name="Shivani";
+	int rollno=16;
+	string class="MCA";
+	int reg=82;
+	printf(" Name",&name);
+	printf("Rollno",&rollno);
+	printf("Class",&class);
+	printf("registrationno",&reg);
+
+}
+
+
+
+	
