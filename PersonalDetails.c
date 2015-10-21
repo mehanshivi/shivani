68294d5c4c342eb8305a2b8ce7cf0572911d699d
shivani <shivanimehan2@gmail.com>
2015-10-21 16:41:08 +0530

    adding new variable

--- PersonalDetails.c
+++ PersonalDetails.c
@@ -4,12 +4,14 @@ void inputdata()
 {
 	string name="Shivani";
 	int rollno=16;
+	int section=1511;
 	string class="MCA";
 	int reg=82;
 	printf(" Name",&name);
 	printf("Rollno",&rollno);
 	printf("Class",&class);
 	printf("registrationno",&reg);
+	printf("Section",& section);
 
 }
 
